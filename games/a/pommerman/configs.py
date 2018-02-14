087960699fba256e116abd59113403d5bf21d407
Cinjon <cinjon@nyu.edu>
2018-02-13 22:53:28 -0500

    configs: add a team variant.

--- games/a/pommerman/configs.py
+++ games/a/pommerman/configs.py
@@ -38,3 +38,21 @@ def ffa_v1():
     }
     agent = characters.Agent
     return locals()
+
+
+def team_v0():
+    env = envs.v0.Pomme
+    game_type = envs.utility.GameType.Team
+    env_entry_point = 'envs:v0:Pomme'
+    env_id = 'Pomme-v0'
+    env_kwargs = {
+        'game_type': game_type,
+        'board_size': envs.utility.BOARD_SIZE,
+        'agent_view_size': envs.utility.AGENT_VIEW_SIZE,
+        'num_rigid': envs.utility.NUM_RIGID,
+        'num_wood': envs.utility.NUM_WOOD,
+        'num_items': envs.utility.NUM_ITEMS,
+        'max_steps': envs.utility.MAX_STEPS
+    }
+    agent = characters.Agent
+    return locals()
