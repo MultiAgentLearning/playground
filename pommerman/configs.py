373fbd2850231b7de6bf2c7315bd6b12c2b46d09
cinjon <cinjon.resnick@gmail.com>
2018-03-24 13:05:15 -0400

    configs: fast team v0.

--- pommerman/configs.py
+++ pommerman/configs.py
@@ -83,6 +83,26 @@ def team_v0():
     return locals()
 
 
+def team_v0_fast():
+    """Start up a team config with the default settings."""
+    env = envs.v0.Pomme
+    game_type = envs.utility.GameType.Team
+    env_entry_point = 'pommerman.envs.v0:Pomme'
+    env_id = 'PommeTeamFast-v0'
+    env_kwargs = {
+        'game_type': game_type,
+        'board_size': envs.utility.BOARD_SIZE,
+        'agent_view_size': envs.utility.AGENT_VIEW_SIZE,
+        'num_rigid': envs.utility.NUM_RIGID,
+        'num_wood': envs.utility.NUM_WOOD,
+        'num_items': envs.utility.NUM_ITEMS,
+        'max_steps': envs.utility.MAX_STEPS,
+        'render_fps': 2000,
+    }
+    agent = characters.Agent
+    return locals()
+
+
 def radio_v2():
     """Start up a team radio config with the default settings."""
     env = envs.v2.Pomme
