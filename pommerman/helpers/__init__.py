de8e61547f29a0f9a8cb92cba51b111cf682020e
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2018-03-27 19:04:53 -0400

    Add internal routine for string parsing

---
+++ pommerman/helpers/__init__.py
@@ -0,0 +1,22 @@
+from .. import agents
+
+
+# @NOTE: This routine is only meant for internal usage. API may change without compatibility
+def _make_agent_from_string(agent_string, port=None, docker_env_dict=None):
+    agent_type, agent_control = agent_string.split("::")
+
+    assert agent_type in ["player", "random", "docker", "test"]
+
+    agent_instance = None
+
+    if agent_type == "player":
+        agent_instance = agents.PlayerAgent()
+    elif agent_type == "random":
+        agent_instance = agents.RandomAgent()
+    elif agent_type == "docker":
+        assert port is not None
+        agent_instance = agents.DockerAgent(agent_control, port=port, env_vars=docker_env_dict)
+    elif agent_type == "test":
+        agent_instance = eval(agent_control)()
+
+    return agent_instance
