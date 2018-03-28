276e583e69ae8aa9a5602d84c705f84ed4fae486
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2018-03-27 22:54:52 -0400

    Fix failing pyglet import in headless environments

--- pommerman/agents/player_agent.py
+++ pommerman/agents/player_agent.py
@@ -1,38 +1,41 @@
-from pyglet.window import key
-
 from . import BaseAgent
 from .. import characters
-
-
-CONTROLS = {
-    'arrows': {
-        key.UP: 1,
-        key.DOWN: 2,
-        key.LEFT: 3,
-        key.RIGHT: 4,
-        key.SPACE: 5,
-        key.M: 6  # In Pommerman, this will freeze the game.
-    },
-    'wasd': {
-        key.W: 1,
-        key.S: 2,
-        key.A: 3,
-        key.D: 4,
-        key.E: 5,
-        key.Q: 6  # In Pommerman, this will freeze the game.
-    }
-}
 
 
 class PlayerAgent(BaseAgent):
     """The Player Agent that lets the user control a character."""
 
-    def __init__(self, agent=characters.Bomber, key_control='arrows'):
+    def __init__(self, agent=characters.Bomber, agent_control='arrows'):
         super(PlayerAgent, self).__init__(agent)
 
-        assert key_control in CONTROLS.keys()
+        ##
+        # @NOTE: DONOT move this import outside the constructor. It will
+        # not work in headless environments like a Docker container
+        # and prevents Pommerman from running
+        #
+        from pyglet.window import key
+        self._controls = {
+            'arrows': {
+                key.UP: 1,
+                key.DOWN: 2,
+                key.LEFT: 3,
+                key.RIGHT: 4,
+                key.SPACE: 5,
+                key.M: 6  # In Pommerman, this will freeze the game.
+            },
+            'wasd': {
+                key.W: 1,
+                key.S: 2,
+                key.A: 3,
+                key.D: 4,
+                key.E: 5,
+                key.Q: 6  # In Pommerman, this will freeze the game.
+            }
+        }
+
+        assert agent_control in self._controls.keys()
         self._key_input = {'curr': 0}
-        self._key_control = key_control
+        self._control_type = agent_control
 
     def act(self, obs, action_space):
         return self._key_input['curr']
@@ -42,7 +45,7 @@ class PlayerAgent(BaseAgent):
         return True
 
     def on_key_press(self, k, mod):
-        self._key_input['curr'] = self._controls[self._key_control].get(k, 0)
+        self._key_input['curr'] = self._controls[self._control_type].get(k, 0)
 
     def on_key_release(self, k, mod):
         self._key_input['curr'] = 0
--- pommerman/helpers/__init__.py
+++ pommerman/helpers/__init__.py
@@ -10,7 +10,7 @@ def _make_agent_from_string(agent_string, port=None, docker_env_dict=None):
     agent_instance = None
 
     if agent_type == "player":
-        agent_instance = agents.PlayerAgent()
+        agent_instance = agents.PlayerAgent(agent_control=agent_control)
     elif agent_type == "random":
         agent_instance = agents.RandomAgent()
     elif agent_type == "docker":
