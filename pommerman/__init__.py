MultiAgentLearning/playground

commit cbcb1814938138cec813b922eb0e9c52fad2be21

_agent --> _character, other minor fixes

pommerman/__init__.py
@@ -4,8 +4,6 @@ from . import configs
 from . import utility
 from . import agents
 
-# servers = os.environ.get('PLAYGROUND_BATTLE_SERVERS', ','.join(['http://localhost']*4)).split(',')
-
 registry = None
 
 
@@ -22,19 +20,19 @@ def _register():
         registry.append(config['env_id'])
 
 
+# Register environments with gym
+_register()
+
+
 def make(config_id, agent_list, game_state_file=None):
     assert config_id in registry
     env = gym.make(config_id)
 
-    for idx, _agent in enumerate(agent_list):
-        assert isinstance(_agent, agents.BaseAgent)
+    for id, agent in enumerate(agent_list):
+        assert isinstance(agent, agents.BaseAgent)
         # @NOTE: This is IMPORTANT so that the agent character is initialized
-        _agent.init_agent(idx, env.spec._kwargs['game_type'])
+        agent.init_agent(id, env.spec._kwargs['game_type'])
 
     env.set_agents(agent_list)
     env.set_init_game_state(game_state_file)
     return env
-
-
-# Register environments with gym
-_register()

pommerman/agents/base_agent.py
@@ -4,17 +4,17 @@ from .. import characters
 class BaseAgent:
     """Parent abstract Agent."""
 
-    def __init__(self, agent=characters.Bomber):
-        self._agent = agent
+    def __init__(self, character=characters.Bomber):
+        self._character = character
 
     def __getattr__(self, attr):
-        return getattr(self._agent, attr)
+        return getattr(self._character, attr)
 
     def act(self, obs, action_space):
         raise NotImplementedError()
 
     def init_agent(self, id, game_type):
-        self._agent = self._agent(id, game_type)
+        self._character = self._character(id, game_type)
 
     @staticmethod
     def has_user_input():

pommerman/agents/docker_agent.py
@@ -16,10 +16,10 @@ class DockerAgent(BaseAgent):
                  docker_image,
                  port,
                  server='http://localhost',
-                 agent=characters.Bomber,
+                 character=characters.Bomber,
                  docker_client=None,
                  env_vars=None):
-        super(DockerAgent, self).__init__(agent)
+        super(DockerAgent, self).__init__(character)
 
         self._docker_image = docker_image
         self._docker_client = docker_client or docker.from_env()

pommerman/agents/player_agent.py
@@ -5,13 +5,13 @@ from .. import characters
 class PlayerAgent(BaseAgent):
     """The Player Agent that lets the user control a character."""
 
-    def __init__(self, agent=characters.Bomber, agent_control='arrows'):
-        super(PlayerAgent, self).__init__(agent)
+    def __init__(self, character=characters.Bomber, agent_control='arrows'):
+        super(PlayerAgent, self).__init__(character)
 
         ##
-        # @NOTE: DONOT move this import outside the constructor. It will
+        # @NOTE: DO NOT move this import outside the constructor. It will
         # not work in headless environments like a Docker container
-        # and prevents Pommerman from running
+        # and prevents Pommerman from running.
         #
         from pyglet.window import key
         self._controls = {

pommerman/agents/tensorforce_agent.py
@@ -6,8 +6,8 @@ from . import BaseAgent
 
 class TensorForceAgent(BaseAgent):
     """The TensorForceAgent. Acts through the algorith, not here."""
-    def __init__(self, agent, algorithm):
-        self._agent = agent
+    def __init__(self, character, algorithm):
+        self._character = character
         self.algorithm = algorithm
 
     def act(self, obs, action_space):

commit e50461ece143937e6c9528ebd043135b0376179f

Partially fix run_battle.py, which broke in #40.
What this is still missing, is the parsing of docker env variables to be
passed to the agent. It's missing, because I have no clue about that and
so wouldn't even know how to test my attempt at fixing it :smile:

pommerman/__init__.py
@@ -25,7 +25,8 @@ _register()
 
 
 def make(config_id, agent_list, game_state_file=None):
-    assert config_id in registry
+    assert config_id in registry, "Unknown configuration '{}'. " \
+        "Possible values: {}".format(config_id, registry)
     env = gym.make(config_id)
 
     for id, agent in enumerate(agent_list):

pommerman/cli/run_battle.py
@@ -20,18 +20,25 @@ import time
 import argparse
 import numpy as np
 
+from .. import helpers
 from .. import make
 
 
 def run(args, num_times=1, seed=None):
     config = args.config
-    agents_string = args.agents
     record_pngs_dir = args.record_pngs_dir
     record_json_dir = args.record_json_dir
     agent_env_vars = args.agent_env_vars
     game_state_file = args.game_state_file
 
-    env = make(config, agents_string, agent_env_vars, game_state_file)
+    # TODO: After https://github.com/MultiAgentLearning/playground/pull/40
+    #       this is still missing the docker_env_dict parsing for the agents.
+    agents = [
+        helpers._make_agent_from_string(agent_string, agent_id+1000)
+        for agent_id, agent_string in enumerate(args.agents.split(','))
+    ]
+
+    env = make(config, agents, game_state_file)
 
     if args.record_pngs_dir:
         assert not os.path.isdir(args.record_pngs_dir)