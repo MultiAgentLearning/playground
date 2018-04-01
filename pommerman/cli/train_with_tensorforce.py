38572c6d39581283d1496c19a0fdde0409621b73
Jon Haygood <jonathan.haygood@timeinc.com>
2018-04-01 15:13:31 -0400

    Tensorforce: fix --render='' option

--- pommerman/cli/train_with_tensorforce.py
+++ pommerman/cli/train_with_tensorforce.py
@@ -113,7 +113,7 @@ def main():
     agent = training_agent.initialize(env)
 
     atexit.register(functools.partial(clean_up_agents, agents))
-    wrapped_env = WrappedEnv(env, visualize=True)
+    wrapped_env = WrappedEnv(env, visualize=args.render)
     runner = Runner(agent=agent, environment=wrapped_env)
     runner.run(episodes=10, max_episode_timesteps=2000)
     print("Stats: ", runner.episode_rewards, runner.episode_timesteps, runner.episode_times)
