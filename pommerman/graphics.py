9b4a55baf91f4d5dcc41ccc9ed902017b07f559b
cinjon <cinjon.resnick@gmail.com>
2018-05-31 11:49:11 -0400

    graphics: remove the rerase from the graphics import.

--- pommerman/graphics.py
+++ pommerman/graphics.py
@@ -27,14 +27,9 @@ try:
     layer_foreground = pyglet.graphics.OrderedGroup(1)
     layer_top = pyglet.graphics.OrderedGroup(2)
 except pyglet.canvas.xlib.NoSuchDisplayException as e:
-    print("Import error! You will not be able to render --> %s" % e)
+    print("Import error NSDE! You will not be able to render --> %s" % e)
 except ImportError as e:
-    reraise(
-        prefix="Error occured while running `from pyglet.gl import *`",
-        suffix="Make sure you have OpenGL installed. On Ubuntu, you can run "
-        "'apt-get install python-opengl'. If you're running on a server, you "
-        "may need a virtual frame buffer; something like this should work: "
-        "'xvfb-run -s \"-screen 0 1400x900x24\" python <your_script.py>'")
+    print("Import error GL! You will not be able to render --> %s" % e)
 
 from . import constants
 from . import utility
