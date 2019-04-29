31b88eb7c69a6ddae987c8b0dd86b1f9df1be943
Robin Chauhan <robin@pathwayi.com>
2019-04-28 20:42:23 -0700

    yapf on player_agent_blocking

--- pommerman/agents/player_agent_blocking.py
+++ pommerman/agents/player_agent_blocking.py
@@ -10,35 +10,35 @@ from .. import characters
 from .. import constants
 
 # keypad control codes
-K_PREFIX='\x1b'
-K_RT='[C'
-K_LF='[D'
-K_UP='[A'
-K_DN='[B'
+K_PREFIX = '\x1b'
+K_RT = '[C'
+K_LF = '[D'
+K_UP = '[A'
+K_DN = '[B'
+
 
 class PlayerAgentBlocking(BaseAgent):
     """Block for keyboard input."""
 
     def __init__(self, character=characters.Bomber, agent_control='arrows'):
         super(PlayerAgentBlocking, self).__init__(character)
-        self.agent_control=agent_control
+        self.agent_control = agent_control
 
     def act(self, obs, action_space):
         key = click.getchar()
-        if self.agent_control=='arrows':
-            if key==K_PREFIX+K_RT: return constants.Action.Right.value
-            if key==K_PREFIX+K_LF: return constants.Action.Left.value
-            if key==K_PREFIX+K_UP: return constants.Action.Up.value
-            if key==K_PREFIX+K_DN: return constants.Action.Down.value
-            if key==' ': return constants.Action.Bomb.value
+        if self.agent_control == 'arrows':
+            if key == K_RT + K_PREFIX: return constants.Action.Right.value
+            if key == K_LF + K_PREFIX: return constants.Action.Left.value
+            if key == K_UP + K_PREFIX: return constants.Action.Up.value
+            if key == K_DN + K_PREFIX: return constants.Action.Down.value
+            if key == ' ': return constants.Action.Bomb.value
             return constants.Action.Stop.value
 
-        if self.agent_control=='wasd':
-            if key=='d': return constants.Action.Right.value
-            if key=='a': return constants.Action.Left.value
-            if key=='w': return constants.Action.Up.value
-            if key=='s': return constants.Action.Down.value
-            if key=='e': return constants.Action.Bomb.value
-            if key=='q': return constants.Action.Stop.value
+        if self.agent_control == 'wasd':
+            if key == 'd': return constants.Action.Right.value
+            if key == 'a': return constants.Action.Left.value
+            if key == 'w': return constants.Action.Up.value
+            if key == 's': return constants.Action.Down.value
+            if key == 'e': return constants.Action.Bomb.value
+            if key == 'q': return constants.Action.Stop.value
             return constants.Action.Stop.value
-
