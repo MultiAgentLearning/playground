ad4f0f3b706714aa7cc8d7795b55fac17e885594
Michael Swan <RMichaelSwan@gmail.com>
2019-11-19 11:57:10 -0800

    got training working from an existing checkpoint.

---
+++ examples/simple_team_run_CNNskynet.py
@@ -0,0 +1,67 @@
+'''An example to show how to set up an pommerman game programmatically'''
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import pommerman
+from pommerman import agents
+from pommerman.agents.nn_agent import NNAgent
+from pommerman.agents.cnn_model import CNNBatchNorm
+
+import torch
+
+def main():
+    '''Simple function to bootstrap a game.
+       
+       Use this as an example to set up your training env.
+    '''
+    # Print all possible environments in the Pommerman registry
+    print(pommerman.REGISTRY)
+
+    # Create a set of agents (exactly four)    
+
+    shape=(14,11,11)
+    n_actions=6
+    n_filters_per_layer=64
+    n_cnn_layers=4
+    nn_model=CNNBatchNorm(input_feature_shape=shape, n_actions=n_actions, n_filters_per_layer=n_filters_per_layer, n_cnn_layers=n_cnn_layers)
+    nn_path='../output/NN_MODELS/ppo_CNN4_64_957.pt'
+    nn_model.load_state_dict(torch.load(nn_path, map_location=lambda storage, loc: storage))
+    selection='softmax'
+    nn_agent = NNAgent(nn_model, action_selection=selection, is_training=False)
+    #nn_agent2 = NNAgent(nn_model, action_selection=selection, is_training=False)
+    
+    idx=0
+    team_id=(idx+2)%4
+    #env_id="PommeFFACompetition-v0"
+    env_id="PommeTeamCompetition-v0"
+    agent_list = [
+        agents.RandomAgent(),
+        agents.RandomAgent(),
+        agents.RandomAgent(),
+        agents.SimpleAgent(),
+        #agents.RandomAgent(),
+    ]
+    agent_list[idx]=nn_agent
+    #agent_list[team_id]=nn_agent2
+    # Make the environment using the agent list
+    env = pommerman.make(env_id, agent_list)
+
+    # Run the episodes just like OpenAI Gym
+    for i_episode in range(1):
+        state = env.reset()
+        done = False
+        while not done:
+            #env.render()
+            actions = env.act(state)
+            #a=nn_agent.act(state[idx], env.action_space, 'softmax') if nn_agent.is_alive else 0
+            #actions[idx]=a
+            print('actions', actions, 'nn alive', nn_agent.is_alive)
+            state, reward, done, info = env.step(actions)
+            #if nn_agent.is_alive ==False: print('dead')
+        print('Episode {} finished'.format(i_episode))
+        print("Final Result: ", info)
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
--- train.sh
+++ train.sh
@@ -9,6 +9,7 @@
 #params are defined in pommerman/agents/nn_model/optimize_nn.py
 #params should contain:
 #n_workers
+#start_iteration --note if not blank/0, you should have a checkpoint file (.pt) in your nn_model_dir that is = start_iteration - 1
 #max_loop_step
 #game_dir
 #nn_model_dir
@@ -38,6 +39,9 @@ max_loop_step=0
 game_dir="GAMES"
 nn_model_dir="NN_MODELS"
 env_id="PommeTeamCompetition-v0"
+start_iteration=0
+
+save_param=true
 
 params=''
 while IFS=":" read key data
@@ -46,8 +50,13 @@ do
     if [[ ${key} == 'n_workers' ]]; then
         n_workers=${data}
     fi
+    if [[ ${key} == 'start_iteration' ]]; then
+        start_iteration=${data}
+        save_param=false
+    fi
     if [[ ${key} == 'max_loop_step' ]]; then
         max_loop_step=${data}
+	max_loop_step=$((max_loop_step + start_iteration))
     fi
     if [[ ${key} == 'game_dir' ]]; then
         game_dir=${data}
@@ -58,12 +67,17 @@ do
     if [[ ${key} == 'env_id' ]]; then
         env_id=${data}
     fi
-    a="--${key}=${data} "
-    params=$params$a 
+
+    if [ "$save_param" = true ] ; then
+        a="--${key}=${data} "
+        params=$params$a
+    fi
+    save_param=true
 done < $param_config_file
 
 echo "---all params: $params"
 echo "n_workers: $n_workers"
+echo "start_iteration: $start_iteration"
 echo "max_loop_step: $max_loop_step"
 echo "game_dir: $game_dir"
 echo "nn_model_dir: $nn_model_dir"
@@ -92,7 +106,7 @@ play_games() {
 }
     
 
-for ((ite=0; ite<$max_loop_step; ite++)) do
+for ((ite=$start_iteration; ite<$max_loop_step; ite++)) do
     echo "iteration $ite"
     play_games $n_workers
     
